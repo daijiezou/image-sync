369958410f7536e62f5b8c84074c184f6c7f462e
daijun <daijun@virtaitech.com>
2024-09-20 10:22:25 +0800

    update source image status

--- update/update_image_metadata.go
+++ update/update_image_metadata.go
@@ -25,9 +25,38 @@ func UpdateImageMeta() {
 			Status:     2, // 2:offline
 			SyncStatus: 3, // 3:已同步回中控
 		}
-		_, err := dao.MySQL().Insert(&imageMeta)
+		if config.IMConfig.TargetAzId == centralAz {
+			imageMeta.Status = 1 // 1:online
+		}
+		has, err := dao.MySQL().Where("name = ?", imageMeta.Name).And("tag = ?", imageMeta.Tag).Get(new(model.ImageMetadata))
+		if err != nil {
+			glog.Error("get image meta failed", glog.String("error", err.Error()), glog.String("image", image.Name+":"+image.Tag))
+		}
+		if has {
+			glog.Infof("image meta already exists", glog.String("image", image.Name+":"+image.Tag))
+			continue
+		}
+		_, err = dao.MySQL().Insert(&imageMeta)
 		if err != nil {
 			glog.Error("insert image meta failed", glog.String("error", err.Error()), glog.String("image", image.Name+":"+image.Tag))
 		}
 	}
+
+	if config.IMConfig.TargetAzId == centralAz {
+		for _, image := range imageList {
+			imageMeta := model.ImageMetadata{
+				Name:       image.Name,
+				Tag:        image.Tag,
+				SyncStatus: 3, // 3:已同步回中控
+			}
+			_, err := dao.MySQL().Cols("sync_status").
+				Where("name = ?", imageMeta.Name).
+				And("tag = ?", imageMeta.Tag).
+				And("az_id = ?", config.IMConfig.SourceAzId).Update(imageMeta)
+			if err != nil {
+				glog.Error("update image meta failed", glog.String("error", err.Error()), glog.String("image", image.Name+":"+image.Tag))
+			}
+		}
+	}
+
 }
